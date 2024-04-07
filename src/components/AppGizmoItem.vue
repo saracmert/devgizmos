29a08edb6c87f0e0bb974f95f704b751c84942eb
Mert Sarac <me@mertsarac.com>
2024-04-08 02:54:47 +0300

    Update AppGizmoItem.vue

--- src/components/AppGizmoItem.vue
+++ src/components/AppGizmoItem.vue
@@ -12,7 +12,7 @@ export default {
   <div class="col-4">    
     <div class="card border p-3 mb-3">
       <div class="card-body">
-        <h5 class="card-title">{{ title }}</h5>
+        <h4 class="card-title">{{ title }}</h4>
         <p class="card-text">{{ description }}</p>
         <a v-bind:href="'/' + gizmo" class="btn btn-primary">Try {{ title }} Gizmo</a>
       </div>
